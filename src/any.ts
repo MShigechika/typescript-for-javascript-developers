9323a355cb176afa13be910f40a7832c071df25e
pkz10 <pkz10@yahoo.co.jp>
2022-05-04 18:49:24 +0900

    add src

---
+++ src/any.ts
@@ -0,0 +1,28 @@
+export {};
+
+import axios from 'axios';
+
+let url: string = 'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
+
+axios.get(url)
+  .then(function (response) {
+    // let data: any;
+    // let data: object[];
+    interface Article {
+      id: number;
+      title: string;
+      description: string;
+    }
+    let data: Article[];
+    data = response.data;
+
+    // ダミーで変数を上書き
+    // data = [
+    //   {
+    //     id: 1,
+    //     title: 'title',
+    //     description: 'description'
+    //   }
+    // ]
+    console.log(data);
+  });
