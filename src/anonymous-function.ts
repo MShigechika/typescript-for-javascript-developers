1eead0a7d493735a0fed51c25977c4496494d19f
pkz10 <pkz10@yahoo.co.jp>
2022-05-08 20:19:05 +0900

    update src/anonymous-function.ts

--- src/anonymous-function.ts
+++ src/anonymous-function.ts
@@ -1,7 +1,10 @@
-export {};
+export {}
 
-let bmi: (height: number, weight: number) => number = function (height: number, weight: number): number {
-  return weight / (height * height);
-};
+let bmi: (height: number, weight: number) => number = function (
+  height: number,
+  weight: number
+): number {
+  return weight / (height * height)
+}
 
-console.log(bmi(1.78, 86));
+console.log(bmi(1.78, 86))
